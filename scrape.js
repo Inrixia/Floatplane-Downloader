c42c31d8c77ebfb73c16c1c9d7d8ab2f49d521e9
INSANITY\Inrix <Inrixia>
2017-11-14 19:31:04 +1300

    commented

--- scrape.js
+++ scrape.js
@@ -11,13 +11,11 @@ var fs = require('fs');
 var cookies = 'ips4_IPSSessionFront=##; ips4_device_key=##; ips4_hasJS=true; ips4_ipsTimezone=Pacific/Auckland; ips4_login_key=##; ips4_member_id=##; ips4_pass_hash=##; muxData=mux_viewer_id=##; video_res=1080p'
 var count = 5; // Number of videos to look back through minus 1 (For the floatplane info post) Max 25
 var loadCount = 0;
-//var episode_number_tq = Number(fs.readFileSync('tq_episode.txt', 'utf8'));
-//var episode_number_ltt = Number(fs.readFileSync('ltt_episode.txt', 'utf8'));
-episode_number_ltt = glob.sync("./Videos/*Linus Tech Tips*.mp4").length
+episode_number_ltt = glob.sync("./Videos/*Linus Tech Tips*.mp4").length // Get the current number of "episodes" for each LTT channel
 episode_number_tq = glob.sync("./Videos/*Techquickie*.mp4").length
 episode_number_fp = glob.sync("./Videos/*Floatplane*.mp4").length
-console.log('Episode Numbers', 'LTT:', episode_number_ltt, 'TQ:', episode_number_tq, 'FP:', episode_number_fp)
-episode_number_ltt += 1
+console.log('Episode Numbers', 'LTT:', episode_number_ltt, 'TQ:', episode_number_tq, 'FP:', episode_number_fp) // Just so you can see the current number of videos
+episode_number_ltt += 1 // Increment by one so new videos dont use existing "episode" numbers
 episode_number_tq += 1
 episode_number_fp += 1
 // set some defaults
@@ -30,19 +28,19 @@ req = request.defaults({
 
 // scrape the page
 req.get({
-    url: "https://linustechtips.com/main/forum/91-lmg-floatplane/",
+    url: "https://linustechtips.com/main/forum/91-lmg-floatplane/", // Get the floatplane index page
     headers: {
         Cookie: cookies
     }
   }, function(err, resp, body) {
 	const $ = cheerio.load(body)
-	$('.cTopicList').children().each(function(i, elem) {
-		if (i == count){
+	$('.cTopicList').children().each(function(i, elem) { // For every form post on this page run the code below
+		if (i == count){ // If we have gone over count ammount of posts stop
 			return false;
-		} else if (i != 0) {
-			var title = $(this).children().eq(1).children().eq(0).children().eq(0).children().eq(0).attr('title')
-			var url = $(this).children().eq(1).children().eq(0).children().eq(0).children().eq(0).attr('href')
-			match = title.replace(/:/g, ' -')
+		} else if (i != 0) { // We dont want to try and get video info from the first post which is the sticky help thread
+			var title = $(this).children().eq(1).children().eq(0).children().eq(0).children().eq(0).attr('title') // Get the title of the video from the post title
+			var url = $(this).children().eq(1).children().eq(0).children().eq(0).children().eq(0).attr('href') // Get the url of the form post containing the video
+			match = title.replace(/:/g, ' -') // Clean up the title for matching against existing videos to determine if they are already downloaded
 			if(match.indexOf('LTT') > -1) {
 				match = match.replace('LTT', 'Linus Tech Tips - S01E'+episode_number_ltt)
 			} else if (match.indexOf('TQ') > -1){
@@ -52,8 +50,8 @@ req.get({
 			}
 			match = sanitize(match);
 			match = match.replace(/^.*[0-9].- /, '').replace('.mp4','')
-			glob("./Videos/*"+match+"*.mp4", function (er, files) {
-  				if (files.length > 0) {
+			glob("./Videos/*"+match+"*.mp4", function (er, files) { // Check if the video already exists
+  				if (files.length > 0) { // If it does then format the title nicely, log that it exists in console and end for this video
   					return_title = title.replace(/:/g, ' -')
   					if(return_title.indexOf('LTT') > -1) {
 						return_title = return_title.replace('LTT', 'Linus Tech Tips')
@@ -63,7 +61,7 @@ req.get({
 						return_title = return_title.replace(/.*FP.*-/, 'Floatplane Exclusive - ')
 					}
 			        console.log(sanitize(return_title), '== EXISTS');
-			    } else {
+			    } else { // If it dosnt exist then format the title with the proper incremented episode number and log that its downloading in console
 			    	title = title.replace(/:/g, ' -')
 					if(title.indexOf('LTT') > -1) {
 						title = title.replace('LTT', 'Linus Tech Tips - S01E'+episode_number_ltt)
@@ -77,30 +75,30 @@ req.get({
 					}
 					title = sanitize(title);
 			    	console.log(title, '== DOWNLOADING');
-			    	loadCount = i;
-					req.get({
+			    	loadCount = i; // Set loadcount to be equal to the current video number (This is the number of the form post from top down, eg video 5 = the 5th post)
+					req.get({ // Open the form post for that video
 				    	url: url,
 					    headers: {
 					        Cookie: cookies
 					     }
 					}, function(err, resp, body) {
 						const $ = cheerio.load(body)
-						var vidID = $('.floatplane-script').attr('data-video-guid');
-						var url = 'https://linustechtips.com/main/applications/floatplane/interface/video_url.php?video_guid='+ vidID +'&video_quality=1080&download=1';
-						var img_url = 'https://cms.linustechtips.com/get/thumbnails/by_guid/'+vidID;
-						request(img_url).pipe(fs.createWriteStream(__dirname + '/Videos/'+title+'.png'));
-						req.get({
+						var vidID = $('.floatplane-script').attr('data-video-guid'); // Get the video ID from the post
+						var url = 'https://linustechtips.com/main/applications/floatplane/interface/video_url.php?video_guid='+ vidID +'&video_quality=1080&download=1'; // Generate the url that will return the download url for this video (To change the quality change 1080 to the resoloution you want)
+						var img_url = 'https://cms.linustechtips.com/get/thumbnails/by_guid/'+vidID; // Generate the url for getting the thumbnail for the video
+						request(img_url).pipe(fs.createWriteStream(__dirname + '/Videos/'+title+'.png')); // Save the thumbnail with the same name as the video so plex will use it
+						req.get({ // Request the download url using the url generated above
 					    	url: url,
 						    headers: {
 						    	Cookie: cookies
 						    }
 						}, function(err, resp, body) {
-				 			progress(request(body), {
+				 			progress(request(body), { // body contains only the download url given from requesting the generated url, Send a request with the download url to start downloading the video
 				 			}).on('progress', function (state) {
-				 				if (loadCount == i) {
+				 				if (loadCount == i) { // If at the last video of the ones downloading clear the screen
 				 					console.log('\x1Bc');
 				    			}
-				    			console.log('Downloading:', title);
+				    			console.log('Downloading:', title); // Below is just printing out the download info
 				    			console.log('Downloaded:', (state.percent*100).toFixed(2) + '%');
 				    			console.log('Time Remaining:', state.time.remaining + 's');
 				    			if (state.speed != null) {
@@ -110,12 +108,12 @@ req.get({
 							}).on('error', function (err) {
 				    			console.log(err);
 							}).on('end', function () {
-							}).pipe(fs.createWriteStream('./Videos/'+title+'.mp4')).on('finish', function(){
+							}).pipe(fs.createWriteStream('./Videos/'+title+'.mp4')).on('finish', function(){ // Save the downloaded video using the title generated
 								file = './Videos/'+title+'.mp4'
-								name = file.replace(/^.*[0-9].- /, '').replace('- ', '').replace('.mp4','')
-								file2 = ('TEMP_'+title+'.mp4').replace(/ /g, '')
-								ffmpeg(file).outputOptions("-metadata", "title="+name, "-map", "0", "-codec", "copy").saveToFile(file2).on('end', function() {
-									mv(file2, file, function(err){
+								name = file.replace(/^.*[0-9].- /, '').replace('- ', '').replace('.mp4','') // Generate the name used for the title in metadata (This is for plex so "episodes" have actual names over Episode1...)
+								file2 = ('TEMP_'+title+'.mp4').replace(/ /g, '') // Specify the temp file to write the metadata to
+								ffmpeg(file).outputOptions("-metadata", "title="+name, "-map", "0", "-codec", "copy").saveToFile(file2).on('end', function() { // Save the title in metadata
+									mv(file2, file, function(err){ // Overwrite the old video with the new one with the updated title
 										if (err) console.error("Error moving file...", err);
 									})
 								})
