a4e0a75350fda8d928bf29f0f0caeb0bfdcc57d3
Inrix <Inrixia@gmail.com>
2020-12-24 20:15:48 +1300

    Fixed requiredPrompts not returning correct values onCancel

--- src/lib/prompts/helpers.ts
+++ src/lib/prompts/helpers.ts
@@ -6,9 +6,8 @@ import prompts from "prompts";
  * @param maxDepth Maximum times to ask the question 
  * @param cancelPrompt String to sent to stdout when user cancels (omitted on last cancel).
  */
-export const requiredPrompts = (question: prompts.PromptObject, maxDepth=2, cancelPrompt="\nAnswering this question is required to continue.\n", depth=0): Promise<prompts.Answers<string>> => {
+export const requiredPrompts = (question: prompts.PromptObject, maxDepth=2, cancelPrompt="\nAnswering this question is required to continue.\n", depth=0): Promise<prompts.Answers<string>> => new Promise(res => {
 	if (depth > 0 && depth < maxDepth) process.stdout.write(cancelPrompt);
 	if (depth >= maxDepth) process.exit(1);
-	const onCancel = () => new Promise(res => res(requiredPrompts(question, maxDepth, cancelPrompt, depth+=1)));
-	return prompts(question, { onCancel });
-};
+	prompts(question, { onCancel: () => res(requiredPrompts(question, maxDepth, cancelPrompt, depth+=1)) }).then(res);
+});
