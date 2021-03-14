26650de8b5c7b0cded90335e98177a4229d7bd1b
Inrix <Inrixia@gmail.com>
2021-03-14 16:12:44 +1300

    Added summary to video progress & sleep's to error recovery

--- src/downloader.ts
+++ src/downloader.ts
@@ -7,11 +7,16 @@ import { fApi } from "./lib/FloatplaneAPI";
 
 import { Resolution } from "./lib/types";
 
+import { promisify } from "util";
+const sleep = promisify(setTimeout);
+
 type promiseFunction = (f: Promise<void>) => void;
 const videoQueue: {video: Video, res: promiseFunction }[] = [];
 let videosProcessing = 0;
+let videosProcessed = 0;
 
 let mpb: MultiProgressBars;
+let downloadStats: { [key: string]: { totalMB: number, downloadedMB: number, downloadSpeed: number, timeElapsed: number } };
 
 setInterval(() => {
 	while(videoQueue.length > 0 && (settings.downloadThreads === -1 || videosProcessing < settings.downloadThreads)) {
@@ -20,7 +25,7 @@ setInterval(() => {
 		if (task !== undefined) {
 			const processingVideoPromise = processVideo(task.video);
 			task.res(processingVideoPromise);
-			processingVideoPromise.then(() => videosProcessing--);
+			processingVideoPromise.then(() => videosProcessing-- && videosProcessed++);
 		}
 	}
 }, 50);
@@ -29,21 +34,43 @@ export const processVideos = (videos: Video[]): Array<Promise<void>> => {
 	if (videos.length !== 0) {
 		console.log(`> Processing ${videos.length} videos...`);
 		mpb = new MultiProgressBars({ initMessage: "", anchor: "top" });
+		downloadStats = {};
+		videosProcessed = 0;
 	}
 	return videos.map(video => new Promise<void>(res => videoQueue.push({video, res })));
 };
 
+const reset = "\u001b[0m";
+
+const updateSummaryBar = () => {
+	const { totalMB, downloadedMB, downloadSpeed, timeElapsed } = Object.values(downloadStats).reduce((summary, stats) => {
+		for (const key in stats)  { summary[key as keyof typeof stats] += stats[key as keyof typeof stats]; }
+		return summary;
+	}, { totalMB: 0, downloadedMB: 0, downloadSpeed: 0, timeElapsed: 0});
+	// (videos remaining * avg time to download a video)
+	const totalVideos = videoQueue.length+videosProcessed+videosProcessing;
+	const summaryDownloadETA = (videoQueue.length+videosProcessing) * (timeElapsed/videosProcessed);
+	const whitespace = "                        ";
+	const processed  = `Processed:        ${videosProcessed}/${totalVideos}${whitespace}`;
+	const downloaded = `Total Downloaded: ${downloadedMB.toFixed(2)}/${totalMB.toFixed(2)}MB${whitespace}`;
+	const speed      = `Download Speed:   ${(downloadSpeed/1024000).toFixed(2)}Mb/s${whitespace}`;
+	const eta 		 = `Rough ETA:        ${Math.floor(summaryDownloadETA / 60)} minutes${whitespace}`;
+	process.stdout.write("                                                         ");
+	process.stdout.write(`\n${processed}\n${downloaded}\n${speed}\n${isNaN(summaryDownloadETA)?"":eta}`);
+};
+
 const processVideo = async (video: Video, retries = 0, quality: Resolution = settings.floatplane.videoResolution) => {
-	const coloredTitle = `${video.channel.title} - ${video.title}`.slice(0, 32);
-	mpb.addTask(coloredTitle, { 
+	const channelColor = settings.colourList[video.channel.title]||"";
+	const coloredTitle = `${channelColor}${video.channel.title}${reset} - ${video.title}`.slice(0, 32+channelColor.length+reset.length);
+	mpb.addTask(coloredTitle, {
 		type: "percentage", 
-		barColorFn: str => `${settings.colourList[video.channel.title]||""}${str}` 
+		barColorFn: str => `${channelColor}${str}`
 	});
 	try {
 		// If the video is already downloaded then just mux its metadata
 		if (!await video.isDownloaded()) {
 			const startTime = Date.now();
-			const downloadRequest = await video.download(fApi, quality);
+			const downloadRequest = await video.download(fApi, quality.toString());
 			downloadRequest.on("downloadProgress", downloadProgress => {
 				const totalMB = downloadProgress.total/1024000;
 				const downloadedMB = (downloadProgress.transferred/1024000);
@@ -52,13 +79,17 @@ const processVideo = async (video: Video, retries = 0, quality: Resolution = set
 				const downloadETA = (downloadProgress.total / downloadSpeed) - timeElapsed;  // Round to 4 decimals
 				mpb.updateTask(coloredTitle, { 
 					percentage: downloadProgress.percent, 
-					message: `${downloadedMB.toFixed(2)}/${totalMB.toFixed(2)}MB, ${(downloadSpeed/1024000).toFixed(2)}Mb/s ETA: ${Math.floor(downloadETA / 60)}m ${Math.floor(downloadETA) % 60}s`
+					message: `${reset}${downloadedMB.toFixed(2)}/${totalMB.toFixed(2)}MB, ${(downloadSpeed/1024000).toFixed(2)}Mb/s ETA: ${Math.floor(downloadETA / 60)}m ${Math.floor(downloadETA) % 60}s`
 				});
+				downloadStats[coloredTitle] = { totalMB, downloadedMB, downloadSpeed, timeElapsed: 0 };
+				updateSummaryBar();
 			});
 			await new Promise((res, rej) => {
 				downloadRequest.on("end", res);
 				downloadRequest.on("error", rej);
 			});
+			downloadStats[coloredTitle].timeElapsed = (Date.now() - startTime) / 1000;
+			downloadStats[coloredTitle].downloadSpeed = 0;
 		}
 		mpb.updateTask(coloredTitle, { 
 			percentage: 0.99, 
@@ -73,12 +104,14 @@ const processVideo = async (video: Video, retries = 0, quality: Resolution = set
 		// Retry downloading the video if this is the first failure.
 		if (retries === 0) {
 			mpb.updateTask(coloredTitle, { message: `\u001b[31m\u001b[1mERR\u001b[0m: ${error.message} - Retrying...` });
+			await sleep(1000);
 			await processVideo(video, ++retries);
 		}
 
 		// If the server aborted the request retry up to 3 times.
 		if (error.message.includes("The server aborted pending request") && retries < 3) {
 			mpb.updateTask(coloredTitle, { message: `\u001b[31m\u001b[1mERR\u001b[0m: ${error.message} - Retrying ${retries}/3` });
+			await sleep(1000);
 			await processVideo(video, ++retries);
 		}
 
@@ -88,6 +121,7 @@ const processVideo = async (video: Video, retries = 0, quality: Resolution = set
 			if (currentResIndex !== 0) {
 				const newRes = settings.floatplane._avalibleResolutions[currentResIndex-1];
 				mpb.updateTask(coloredTitle, { message: `\u001b[31m\u001b[1mERR\u001b[0m: ${error.message} - Retrying at ${newRes}p` });
+				await sleep(1000);
 				await processVideo(video, retries, newRes);
 			}
 		}
