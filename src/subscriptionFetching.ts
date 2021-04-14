4594ac01a9ba8313d1be9b5010bf6db67aeabbe8
Inrix <Inrixia@gmail.com>
2021-04-14 13:25:31 +1200

    Refactored subscription fetching to only deal with fetching subscriptions

--- src/subscriptionFetching.ts
+++ src/subscriptionFetching.ts
@@ -1,7 +1,3 @@
-import type { Subscription as fApiSubscription } from "floatplane/user";
-import type FloatplaneApi from "floatplane";
-import type Video from "./lib/Video";
-
 import { settings } from "./lib/helpers";
 import { defaultSubChannels } from "./lib/defaults";
 import Subscription from "./lib/Subscription";
@@ -32,33 +28,3 @@ export const fetchSubscriptions = async (): Promise<Subscription[]> => (await fA
 		};
 		return new Subscription(settings.subscriptions[subscription.creator]);
 	});
-
-export const fetchSubscriptionVideos = async (subscription: fApiSubscription, fApi: FloatplaneApi): Promise<Video[]> => {
-	const lastSeenVideo = sub.lastSeenVideo.videoGUID;
-
-	// Search infinitely if we are resuming. Otherwise only grab the latest `settings.floatplane.videosToSearch` videos
-	const videosToSearch = settings.floatplane.videosToSearch;
-
-	let videosSearched = 0;
-	let foundLastSeenVideo = false;
-	const videos = [];
-	
-	const coloredTitle = `${settings.channelColors[titleAlias]||"\u001b[38;5;208m"}${titleAlias}\u001b[0m`;
-	process.stdout.write(`> Fetching latest videos from [${coloredTitle}]... Fetched ${videos.length} videos!`);
-	for await (const video of fApi.creator.blogPostsIterable(subscription.creator, { type: "video" })) {
-		if (video.guid === lastSeenVideo || lastSeenVideo === "") foundLastSeenVideo = true;
-		if (videosSearched >= videosToSearch && foundLastSeenVideo) break;
-		videos.push(video);
-		videosSearched++;
-		process.stdout.write(`\r> Fetching latest videos from [${coloredTitle}]... Fetched ${videos.length} videos!`);
-	}
-	process.stdout.write("\n");
-
-	// Make sure videos are in correct order for episode numbering, null episodes are part of a channel that is marked to be skipped
-	const incompleteVideos: Video[] = [];
-	for (const video of videos.sort((a, b) => (+new Date(a.releaseDate)) - (+new Date(b.releaseDate))).map(sub.addVideo)) {
-		if (video !== null && !await video.isMuxed()) incompleteVideos.push(video);
-	}
-	process.stdout.write(` Skipped ${videos.length-incompleteVideos.length}.\n`);
-	return incompleteVideos;
-};
