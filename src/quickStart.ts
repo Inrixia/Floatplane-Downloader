6bce657513c4e0d8e547c4120869629c592da8b0
Inrix <Inrixia@gmail.com>
2021-11-06 18:47:24 +1300

    Moved Floatplane login prompt to the start of quickStartPrompts

--- src/quickStart.ts
+++ src/quickStart.ts
@@ -43,8 +43,15 @@ export const validatePlexSettings = async (): Promise<void> => {
 export const quickStart = async (): Promise<void> => {
 	console.log('Welcome to Floatplane Downloader! Thanks for checking it out <3.');
 	console.log('According to your settings.json this is your first launch! So lets go through the basic setup...\n');
-	console.log('\n== \u001b[38;5;208mGeneral\u001b[0m ==\n');
 
+	console.log('\n== \u001b[38;5;208mFloatplane\u001b[0m ==\n');
+	// Dont re-prompt for credentials if we are already logged in
+	if ((await fApi.isAuthenticated()) !== true) {
+		console.log('Please login to floatplane...');
+		await loginFloatplane();
+	} else console.log('Already logged in!');
+
+	console.log('\n== \u001b[38;5;208mGeneral\u001b[0m ==\n');
 	settings.floatplane.videosToSearch = await prompts.floatplane.videosToSearch(settings.floatplane.videosToSearch);
 	settings.downloadThreads = await prompts.settings.downloadThreads(settings.downloadThreads);
 	settings.floatplane.videoResolution = await prompts.settings.videoResolution(settings.floatplane.videoResolution, defaultResolutions);
@@ -54,12 +61,6 @@ export const quickStart = async (): Promise<void> => {
 	if (extras !== undefined) {
 		for (const extra in settings.extras) settings.extras[extra as keyof Extras] = extras.indexOf(extra) > -1 ? true : false;
 	}
-
-	console.log('\n== \u001b[38;5;208mFloatplane\u001b[0m ==\n');
-	console.log('Next we are going to login to floatplane...');
-	// Dont re-prompt for credentials if we are already logged in
-	if ((await fApi.isAuthenticated()) !== true) await loginFloatplane();
-	else console.log('Already logged in!');
 
 	console.log('\n== \u001b[38;5;208mPlex\u001b[0m ==\n');
 	settings.plex.enabled = await prompts.plex.usePlex(settings.plex.enabled);
