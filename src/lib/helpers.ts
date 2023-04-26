c967adb3069d90664ef6a8cf59544388ca406a8c
Inrixia <Inrixia@gmail.com>
2023-04-26 12:45:41 +1200

    Updated version to 5.8.0

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -14,7 +14,7 @@ import "dotenv/config";
 import json5 from "json5";
 const { parse } = json5;
 
-export const DownloaderVersion = "5.7.0";
+export const DownloaderVersion = "5.8.0";
 
 import type { PartialArgs, Settings } from "./types.js";
 
